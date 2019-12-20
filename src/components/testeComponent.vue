b4008a1c77ead6a9efd73c5da59d49fef10164c5
R4YC0NLima <49953505+R4YC0NLima@users.noreply.github.com>
2019-12-20 12:43:12 -0400

    create and use testeComponent in vue-cli

---
+++ src/components/testeComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="comp">
+        <h1> {{ title }} </h1>
+        <p>ao</p>
+        <span>curso de {{ msg }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "teste-component",
+        data()
+        {
+            return{
+                title: 'teste Component',
+                msg: 'VueJs'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .comp{
+        width: 98%;
+        background-color: crimson;
+        border: 1px solid purple;
+        margin: 5px;
+        padding: 5px;
+    }
+</style>
