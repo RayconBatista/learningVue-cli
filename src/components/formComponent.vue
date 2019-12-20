303b3a03b847e0e7bc64595600a69620e033061e
R4YC0NLima <49953505+R4YC0NLima@users.noreply.github.com>
2019-12-20 12:41:39 -0400

    formComponents

---
+++ src/components/formComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="comp">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "formComponent",
+        data()
+        {
+            return{
+                title: '#form component'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .comp{
+        background-color: yellow;
+        border: 1px solid yellowgreen;
+        margin: 5px;
+        padding: 5px;
+    }
+
+</style>
