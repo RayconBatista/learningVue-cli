7b2efe0caef29af30d863031811e75ba211a6cd1
Raycon <rayconlimabatista@gmail.com>
2019-12-20 10:53:08 -0400

    Leaving hello world to test any vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Teste - {{ msg}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      msg: 'Alguma coisa'
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
