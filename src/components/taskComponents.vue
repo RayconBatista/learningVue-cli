afa5e055d7fabcfd6561a3972b3ab5657600c8a0
R4YC0NLima <49953505+R4YC0NLima@users.noreply.github.com>
2019-12-20 12:42:22 -0400

    create and use taskComponents in Vue-cli

---
+++ src/components/taskComponents.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="comp">
+        <h1>{{title}}</h1>
+        <table class="borderTable">
+            <tr>
+                <th>nome</th>
+                <th>Completa?</th>
+            </tr>
+            <tr v-for="(task, index) in tasks" :key="index">
+                <td>{{ task.name }}</td>
+                <td>{{ task.completed ? 'Sim' : 'Não'}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "taskComponents",
+        data()
+        {
+            return{
+                tasks:[
+                    {name: 'Lavar louça', completed: false},
+                    {name: 'Lavar banheiro', completed: true},
+                    {name: 'Lavar carro', completed: false},
+                    {name: 'Lavar moto', completed: true},
+                ],
+                title: 'Tasks Components'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .comp{
+        background-color: dodgerblue;
+        border: 1px solid green;
+        width: 15vw;
+    }
+
+    table.borderTable{
+        border-collapse: collapse;
+        background: aliceblue;
+    }
+
+    table.borderTable td{
+        border: 1px solid black;
+    }
+
+    table.borderTable th{
+        border: 1px solid black;
+    }
+</style>
