87b3bf11b5f74aba575b5832d17da9749e88875e
R4YC0NLima <49953505+R4YC0NLima@users.noreply.github.com>
2019-12-20 12:44:42 -0400

    update file App.vue for vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
-  <div id="app">
-    Teste - {{ msg}}
+  <div id="app" class="comp">
+    <h1> {{ title }}</h1>
+    <p>Bem vindo</p>
+    <teste-component></teste-component>
+    <br>
+    <form-component></form-component>
+    <hr>
+    <task-components></task-components>
   </div>
 </template>
 
 <script>
+import testeComponent from "./components/testeComponent";
+import formComponent from "./components/formComponent";
+import taskComponents from "./components/taskComponents";
 
 export default {
   name: 'app',
-  data(){
-    return {
-      msg: 'Alguma coisa'
-    }
-  },
   components: {
+    testeComponent,
+    formComponent,
+    taskComponents
+  },
+  data()
+  {
+    return{
+      title: 'app component'
+    }
   }
 }
 </script>
 
 <style>
+
+.comp{
+  background-color: #42b983;
+  border: 1px solid blue;
+  margin: 5px;
+  padding: 5px;
+}
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
